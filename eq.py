814116d2f9281f6f613cfb5ed4b4c61c04fa88d8
neo <neo@matrix.com>
2022-07-31 15:24:09 +0530

    Added code for json data

--- eq.py
+++ eq.py
@@ -1,42 +1,43 @@
-followers = set();
-following = set();
+import json
+
+followers = set()
+following = set()
 
 p1 = []
 p2 = []
 
-# extracting followers list
-with open("followersRaw.txt","rt") as f:
-    i = 0;
-    for str in f:
-        if i%2 == 0:
-           str = str[:-1]
-           followers.add(str);
-        i = i+1;
-# print("Follower List:")
-# print(followers);
+# playground
+with open("./followers.json", "r") as f:
+    data = json.load(f)
+    # print(data["relationships_followers"])
+    # print(type(data["relationships_followers"]))
+    for p in data["relationships_followers"]:
+        # print(type(p))
+        # p = json.loads(p)
+        uname = p["string_list_data"][0]["value"]
+        # uname = p["string_list_data"]
+        followers.add(uname)
 
-# extracting following list
-with open("followingRaw.txt","rt") as f:
-    i = 0;
-    for str in f:
-        if i%2 == 0:
-           str = str[:-1]
-           following.add(str);
-        i = i+1;
-# print("Following List: ")
-# print(following);
+with open("./following.json", "r") as f:
+    data = json.load(f)
+    for p in data["relationships_following"]:
+        # print(type(p))
+        # p = json.loads(p)
+        uname = p["string_list_data"][0]["value"]
+        # uname = p["string_list_data"]
+        following.add(uname)
+
 
 for f in following:
-	if f not in followers:
-		p1.append(f);
+    if f not in followers:
+        p1.append(f)
 
 for f in followers:
-	if f not in following:
-		p2.append(f);
+    if f not in following:
+        p2.append(f)
 
-print ("People who don't follow you back:");
-print(p1);
+print("People who don't follow you back:")
+print(p1)
 
-print ("People who you don't follow back:");
-print(p2);
-
+print("People who you don't follow back:")
+print(p2)
